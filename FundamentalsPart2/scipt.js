75bd40fcf1a7297a6b2c1bf10f870fec84f4567a
Can <cunsalan@gmail.com>
2021-10-19 12:37:14 +0300

    fundamentals part 2 commit

---
+++ FundamentalsPart2/scipt.js
@@ -0,0 +1,12 @@
+//'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+const interface = 'Audio';
+
+if(passTest) hasDriverLicense = true;
+
+if(hasDriversLicense) console.log('You are safe!')
+
+
